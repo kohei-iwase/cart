f3cad6717ffea5c17e97b004fc026922d3471ede
Iwase <57283946+kohei-iwase@users.noreply.github.com>
2021-11-01 00:00:37 +0900

    Add files via upload

---
+++ staff_login/staff_top.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charaset="UTF-8">
+<title>ろくまる農園</title>
+</head>
+<body>
+</body>
+ショップ管理トップメニュー<br />
+<br />
+<a href="../staff/staff_list.php">スタッフ管理</a><br />
+<br />
+<a href="../staff/pro_list.php">商品管理</a><br />
+</form>
+<html>
